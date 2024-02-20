3adb9646bb4f9a18e664397890abd9fae15c1cf5
Kush Patel <ktpatel3@myseneca.ca>
2024-02-20 12:48:03 -0500

    Adding Css to the register page

--- app/registeruser/page.js
+++ app/registeruser/page.js
@@ -1,48 +1,11 @@
 "use client";
-// import React from 'react';
-// import styles from '../../styling/registeruser.module.css'; // Import our CSS module
-
-// export default function RegistrationForm(){
-//   return (
-//     <div className={styles.formContainer}>
-//       <form className={styles.registrationForm}>
-//         <h2>User Registration</h2>
-        
-//         <label htmlFor="firstName">First Name*</label>
-//         <input type="text" id="firstName" name="firstName" required />
-        
-//         <label htmlFor="lastName">Last Name*</label>
-//         <input type="text" id="lastName" name="lastName" required />
-        
-//         <label htmlFor="email">Email Address*</label>
-//         <input type="email" id="email" name="email" required />
-        
-//         <label htmlFor="dob">Date of Birth*</label>
-//         <input type="date" id="dob" name="dob" required />
-        
-//         <label htmlFor="phone">Phone Number*</label>
-//         <input type="tel" id="phone" name="phone" required />
-        
-//         <label htmlFor="participation">Will you be running or walking?*</label>
-//         <select id="participation" name="participation" required>
-//           <option value="">Choose One</option>
-//           <option value="running">Running</option>
-//           <option value="walking">Walking</option>
-//         </select>
-        
-//         <button type="submit" className={styles.submitButton}>Continue</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-
-import styles from '../../styling/registeruser.module.css'; 
-import { Card, Form, Alert, Button } from "react-bootstrap";
-import { useState} from "react";
-import { registerUsers } from "../lib/authenticate/authenticate";
+import styles from '../../styling/registeruser.module.css';
+import { Card, Form, Alert, Button, Container, Row,Col } from "react-bootstrap";
+import { useState } from "react";
+import { registerUsers } from "../lib/authenticate";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react"; 
+import { useEffect } from "react";
+
 export default function Register() {
   const [warning, setWarning] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -50,10 +13,10 @@ export default function Register() {
   const [email, setEmail] = useState("");
   const [PSW, setPSW] = useState(false);
   const [password, setPassword] = useState("");
-  const [password2, setPassword2] = useState("");
-  const colorArray = ['warning', 'primary', 'success','danger','info','secondary'];
-  const [bordercolor, setBordercolor]= useState(0);
-  //within 3 seconds border color change from info to primary to success
+  const colorArray = ['warning', 'primary', 'success', 'danger', 'info', 'secondary'];
+  const [bordercolor, setBordercolor] = useState(0);
+
+  //within 3 seconds b  order color change from info to primary to success
   const router = useRouter();
 
   useEffect(() => {
@@ -66,50 +29,80 @@ export default function Register() {
     };
   }, []);
 
-  const currentBordercolor = colorArray[bordercolor];
+ // const currentBordercolor = colorArray[bordercolor];
 
   async function handleSubmit(e) {
     e.preventDefault();
-
     try {
-        await registerUsers(email, password, firstName, lastName, PSW);
-        //router.push("/authenticate");
+      const res = await registerUsers(email, password, firstName, lastName, PSW);
+      console.log("res is:", res);
+      if (res.status === "error" && res.error.message) {
+        console.log("res.error.message is:", res.error.message);
+        setWarning(res.error.message);
+        return;
+      }
+      router.push("/login");
     } catch (err) {
       setWarning(err.message);
     }
   }
 
   return (
-    <div className={`p-5`} style={{ width: '30%' }}>
-      <Card bg="dark"  className={`border border-${currentBordercolor} p-3`}>
-        <Card.Body style={{color:"white"}}><h2>Register:</h2>Register as a new user to avail our services:</Card.Body>
-      </Card>
-      <br />
-      <Form onSubmit={handleSubmit}>
-        <Form.Group>
-          <Form.Label>First Name</Form.Label><Form.Control type="string" value={firstName} id="fname" name="fname" onChange={(e) => setFirstName(e.target.value)}/>
-        </Form.Group>
-        <br />
-        <Form.Group>
-          <Form.Label>Last Name</Form.Label><Form.Control type="string" value={lastName} id="lname" name="lname" onChange={(e) => setLastName(e.target.value)}/>
-        </Form.Group>
-        <br />
-        <Form.Group>
-          <Form.Label>Email:</Form.Label><Form.Control type="email" value={email} id="useremail" name="useremail" onChange={(e) => setEmail(e.target.value)}/>
-        </Form.Group>
-        <br />
-        <Form.Group>
-          <Form.Label>Password:</Form.Label><Form.Control type="password" value={password} id="password" name="password" onChange={(e) => setPassword(e.target.value)}/>
-          {/* <Form.Label>Password:</Form.Label><Form.Control type="password" value={password} id="password" name="password" onChange={(e) => setPassword(e.target.value)}/> */}
-        </Form.Group>
-        {warning && (<><br /><Alert variant="danger">{warning}</Alert></>)}
-        <br />
-        <Form.Group>
-          <Form.Check label="Is PSW" type="checkbox" value={PSW} id="ispsw" name="ispsw" onChange={(e) => setPSW(e.target.checked)}/>
-        </Form.Group>
-        <br />
-        <Button variant="dark" className="pull-right" type="submit">Register</Button>
-      </Form>
-    </div>
+    //  without the container fluid the bottom and horizontal scroll bar will appear
+    <Container fluid> 
+      <Row>
+        <Col className="max-w-xs p-6 bg-gray-100  rounded-lg shadow-md font-bold" xs={12} md={6} lg={4} xl={4} xxl={5}>
+        <div className='w-10/12 justify-center items-center px-20'>
+          <div>
+          <Card>
+            <Card.Body  className='text-slate-100 font-bold'><h2 className='text-xl text-center'>Register:</h2><h3 className='font-semibold text-lg text-center'>Create account to avail services</h3></Card.Body>
+          </Card>
+          <br />
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label className='text-sm font-semibold'>First Name</Form.Label><Form.Control type="string" value={firstName} id="fname" name="fname" onChange={(e) => setFirstName(e.target.value)} />
+            </Form.Group>
+            <br />
+            <Form.Group>
+              <Form.Label className='text-sm font-semibold'>Last Name</Form.Label><Form.Control type="string" value={lastName} id="lname" name="lname" onChange={(e) => setLastName(e.target.value)} />
+            </Form.Group>
+            <br />
+            <Form.Group>
+              <Form.Label className='text-sm font-semibold'>Email:</Form.Label><Form.Control type="email" value={email} id="useremail" name="useremail" onChange={(e) => setEmail(e.target.value)} />
+            </Form.Group>
+            <br />
+            <Form.Group>
+              <Form.Label className='text-sm font-semibold'>Password:</Form.Label><Form.Control type="password" value={password} id="password" name="password" onChange={(e) => setPassword(e.target.value)} />
+              {/* <Form.Label>Password:</Form.Label><Form.Control type="password" value={password} id="password" name="password" onChange={(e) => setPassword(e.target.value)}/> */}
+            </Form.Group>
+            <br />
+            <Form.Group>
+              <Form.Check className='text-sm font-semibold' label="Is PSW" type="checkbox" value={PSW} id="ispsw" name="ispsw" onChange={(e) => setPSW(e.target.checked)} />
+            </Form.Group>
+            {warning && (<><br /><Alert variant="danger">{warning}</Alert></>)}
+            {/* className="pull-right" */}
+            <Button className="mt-5 py-2 px-5 text-white rounded cursor-pointer hover:bg-blue-700" variant="dark" type="submit">Register</Button>
+          </Form>  
+          </div>
+          </div>
+        </Col>
+
+        <Col md={6} lg={8} xl={8} xxl={7}>
+              <div className='flex flex-col justify-center items-center h-full rounded-md mt-2 pt-10 pb-10  bg-emerald-600'>
+                <div className='w-1/2'>
+                  <h2 className='text-4xl text-slate-100 text-center font-bold'>PSW Support and Care</h2>
+                  <p className=' text-slate-100 text-balance text-center font-semibold px-18'>Support Worker is an innovative online company dedicated to providing personal 
+                  support services. Our mission is to offer accessible, high-quality assistance to individuals in need, directly 
+                  through our online platform. By leveraging the power of technology, we connect clients with experienced personal
+                   support workers who deliver compassionate care, tailored to each client's unique needs. Whether it's assistance 
+                   with daily activities, health-related support, or companionship, our goal is to enhance the quality of life for 
+                   those we serve. At Support Worker, we believe in creating a community of care that's just a click away, making
+                    personal support more convenient and effective than ever before.</p>
+                </div>
+            </div>
+        </Col>
+
+      </Row>
+    </Container>
   );
 }
