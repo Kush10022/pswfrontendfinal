e4d0629cb2f3f9efee3aadd412ff1b0eac9d4c11
tsumiksh@gmail.com <96885160+Sumiksh@users.noreply.github.com>
2024-03-14 16:29:05 -0400

    changes calling function in useeffect

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 //if reactStrictMode: false then will run once . By default its true
-const nextConfig = {};
+const nextConfig = {
+    // reactStrictMode: false
+};
 
 export default nextConfig;
