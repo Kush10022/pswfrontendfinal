f2e63a37501dcf4b46eb5cb5ee490bd6ee69e938
Ujjval Patel <upatel69@myseneca.ca>
2024-09-24 23:03:05 -0400

    Enhance Profile Picture Upload with Crop and Error Handling Features

--- app/lib/ProfilePicUploader.js
+++ app/lib/ProfilePicUploader.js
@@ -5,6 +5,7 @@ import Cookies from "js-cookie";
 import { userProfileAtom } from "../atoms/userAtom";
 import { useAtom } from "jotai";
 import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 // Utility to create an image element from a data URL
 const createImage = (url) =>
@@ -12,7 +13,7 @@ const createImage = (url) =>
     const image = new Image();
     image.addEventListener("load", () => resolve(image));
     image.addEventListener("error", (error) => reject(error));
-    image.setAttribute("crossOrigin", "anonymous"); // Needed to avoid cross-origin issues
+    image.setAttribute("crossOrigin", "anonymous"); // Avoid cross-origin issues
     image.src = url;
   });
 
@@ -39,23 +40,22 @@ async function getCroppedImg(imageSrc, pixelCrop) {
     pixelCrop.height
   );
 
-  // Convert the canvas to a data URL
+  // Convert the canvas to a data URL and return the file blob
   return new Promise((resolve, reject) => {
     canvas.toBlob((blob) => {
       if (!blob) {
-        reject(new Error("Canvas is empty"));
+        reject(new Error("Something went wrong while cropping the image"));
         return;
       }
       const buffer = new File([blob], "profilePic.jpeg", {
         type: "image/jpeg",
       });
-
       resolve(buffer);
     }, "image/jpeg");
   });
 }
 
-// Utility function to convert a Blob into a Data URL
+// Utility function to convert a file Blob into a Data URL
 const readFile = (file) => {
   return new Promise((resolve) => {
     const reader = new FileReader();
@@ -71,10 +71,15 @@ const ProfilePicUploader = ({ onDone }) => {
   const [zoom, setZoom] = useState(1);
   const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
   const [user, setUser] = useAtom(userProfileAtom);
+  const [isUploading, setIsUploading] = useState(false);
 
   const onFileChange = async (e) => {
     if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0];
+      if (file.size > 1024 * 1024) {
+        toast.error("Please choose an image smaller than 1MB.");
+        return;
+      }
       let imageDataUrl = await readFile(file);
       setImageSrc(imageDataUrl);
     }
@@ -85,11 +90,21 @@ const ProfilePicUploader = ({ onDone }) => {
   };
 
   const onCrop = async () => {
+    setIsUploading(true);
+    const toastId = toast.loading("Uploading your profile picture...");
     try {
       const croppedImage = await getCroppedImg(imageSrc, croppedAreaPixels);
+      if (!croppedImage) {
+        throw new Error("Could not crop the image. Please try again.");
+      }
+      if (croppedImage.size > 400 * 1024) {
+        throw new Error(
+          "Cropped image exceeds 400KB. Try a smaller size or further crop."
+        );
+      }
       const token = Cookies.get("authToken");
       if (!token) {
-        throw new Error("Token not available");
+        throw new Error("You are not authenticated. Please log in again.");
       }
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/v1/private/user/picture`,
@@ -105,32 +120,45 @@ const ProfilePicUploader = ({ onDone }) => {
 
       if (response.ok) {
         const data = await response.json();
-        let myUser = user;
-        myUser.profilePicture = data.url;
-        setUser(myUser);
+        let updatedUser = user;
+        updatedUser.profilePicture = data.url;
+        setUser(updatedUser);
         router.refresh();
-        onDone();
+        setTimeout(() => {
+          setIsUploading(false);
+          toast.success("Profile picture updated successfully!", {
+            id: toastId,
+          });
+          onDone();
+        }, 1000);
+      } else {
+        throw new Error("Failed to upload the picture. Please try again.");
       }
     } catch (e) {
-      console.error(e);
+      setTimeout(() => {
+        setIsUploading(false);
+        const message =
+          e.message || "An error occurred. Please try again later.";
+        toast.error(message, { id: toastId });
+        setIsUploading(false);
+      }, 1000);
     }
   };
 
   return (
     <div className="flex flex-col items-center justify-center m-5">
-      {!imageSrc && (
-        <input
-          className="m-4 file-input text-sm text-grey-500
+      <input
+        className="m-4 file-input text-sm text-gray-500
                    file:mr-4 file:py-2 file:px-4
                    file:rounded-full file:border-0
                    file:text-sm file:font-semibold
-                   file:bg-violet-50 file:text-violet-700
-                   hover:file:bg-violet-100"
-          type="file"
-          accept="image/*"
-          onChange={onFileChange}
-        />
-      )}
+                   file:bg-blue-50 file:text-blue-700
+                   hover:file:bg-blue-100"
+        type="file"
+        accept="image/*"
+        onChange={onFileChange}
+      />
+
       {imageSrc && (
         <div className="relative w-96 h-96 mt-8 mb-10">
           <Cropper
@@ -143,10 +171,12 @@ const ProfilePicUploader = ({ onDone }) => {
             onZoomChange={setZoom}
           />
           <button
-            className="absolute inset-x-0 -bottom-12 mt-2 py-2 px-2 rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
+            className={`absolute inset-x-0 -bottom-12 mt-2 py-2 px-2 rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700 ${
+              isUploading ? "opacity-35 cursor-not-allowed" : "cursor-pointer"
+            }`}
             onClick={onCrop}
           >
-            Crop Image
+            {isUploading ? "Uploading..." : "Save Changes"}
           </button>
         </div>
       )}
