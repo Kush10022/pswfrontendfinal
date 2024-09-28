1cc7a926187266ef6788239cf39043b264611cb6
Ujjval Patel <upatel69@myseneca.ca>
2024-09-27 22:57:28 -0400

    Refactor: Add SVG components and utility functions for fetching and authenticating users

---
+++ app/lib/utils/assitivefetch.js
@@ -0,0 +1,11 @@
+export async function AssitiveFetch(pathReceived, methodReceived, payloadReceived) {
+    const response = await fetch(pathReceived, {
+        method: methodReceived,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payloadReceived)
+    });
+    const responseData = await response.json();
+    return responseData;
+}
