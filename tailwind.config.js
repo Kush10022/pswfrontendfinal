5938d0e5007c3e04bac6e67b70742be17150af0e
Ujjval Patel <upatel69@myseneca.ca>
2024-10-14 23:14:52 -0400

    Refactor: Update tailwind.config.js to extend theme with new animations
    - Added new animations 'shake' and 'fadeIn' to the theme's 'extend' section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
- 
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
     // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        shake: "shake 0.8s cubic-bezier(.36,.07,.19,.97) both infinite",
+        fadeIn: "fadeIn 1s ease-in-out",
+      },
+      keyframes: {
+        shake: {
+          "10%, 90%": { transform: "translate3d(-1px, 0, 0)" },
+          "20%, 80%": { transform: "translate3d(2px, 0, 0)" },
+          "30%, 50%, 70%": { transform: "translate3d(-4px, 0, 0)" },
+          "40%, 60%": { transform: "translate3d(4px, 0, 0)" },
+        },
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+    },
   },
+
   plugins: [],
-}
+};
